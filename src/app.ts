11362acf20315490223fb9cf1bad643e1db66087
shubhsardana29 <shubhsardana29@gmail.com>
2024-08-06 17:10:56 +0530

    feat: Add route for homepage

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.use('/api/devices', deviceRoutes);
 app.use('/api/updates', updateRoutes);
 
